29da1ea17ce34613757ed55db46387aeff8d56af
Douglas <douglas@mouseweb.com.br>
2021-04-18 02:02:12 -0300

    TDD - parte 1

---
+++ src/main/java/br/com/mouseweb/servicos/Calculadora.java
@@ -0,0 +1,22 @@
+package br.com.mouseweb.servicos;
+
+import br.com.mouseweb.exception.NaoPodeDividirPorZeroException;
+
+public class Calculadora {
+
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public int divide(int a, int b) throws NaoPodeDividirPorZeroException {
+        if(b == 0) {
+            throw new NaoPodeDividirPorZeroException();
+        }
+        return a / b;
+    }
+
+}
