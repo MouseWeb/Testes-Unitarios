adb96d3885663c472a6b4530b193504a2e18a0f7
Douglas <douglas@mouseweb.com.br>
2021-04-18 01:17:00 -0300

    Ordem de execução dos testes

---
+++ src/test/java/br/com/mouseweb/OrdemTest.java
@@ -0,0 +1,24 @@
+package br.com.mouseweb;
+
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+//Anotação define a execução em ordem alfabética
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class OrdemTest {
+
+    public static int contador = 0;
+
+    @Test
+    public void inicia(){
+        contador = 1;
+    }
+
+    @Test
+    public void verifica(){
+        Assert.assertEquals(1, contador);
+    }
+
+}
